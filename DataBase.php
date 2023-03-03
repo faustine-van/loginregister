4e42550abaa829533b72b1d2f9c945983d9f5863
faustinemuhayemariya <faustinemuhayemariya44@gmail.com>
2023-03-03 13:20:52 +0200

    login register

---
+++ DataBase.php
@@ -0,0 +1,73 @@
+<?php
+require "DataBaseConfig.php";
+
+class DataBase
+{
+    public $connect;
+    public $data;
+    private $sql;
+    protected $servername;
+    protected $username;
+    protected $password;
+    protected $databasename;
+
+    public function __construct()
+    {
+        $this->connect = null;
+        $this->data = null;
+        $this->sql = null;
+        $dbc = new DataBaseConfig();
+        $this->servername = $dbc->servername;
+        $this->username = $dbc->username;
+        $this->password = $dbc->password;
+        $this->databasename = $dbc->databasename;
+    }
+
+    function dbConnect()
+    {
+        $this->connect = mysqli_connect($this->servername, $this->username, $this->password, $this->databasename);
+        return $this->connect;
+    }
+
+    function prepareData($data)
+    {
+        return mysqli_real_escape_string($this->connect, stripslashes(htmlspecialchars($data)));
+    }
+
+    function logIn($table, $username,$email, $password)
+    {
+        $username = $this->prepareData($username);
+        $password = $this->prepareData($password);
+        $email=$this->prepareData($email);
+        $this->sql = "select * from " . $table . " where username = '" . $username . "' && email=='".$email."'" ;
+        $result = mysqli_query($this->connect, $this->sql);
+        $row = mysqli_fetch_assoc($result);
+        if (mysqli_num_rows($result) != 0) {
+            $dbusername = $row['username'];
+            $dbpassword = $row['password'];
+            $dbemail = $row['email'];
+            if ($dbusername == $username && $dbemail==$email && password_verify($password, $dbpassword)) {
+                $login = true;
+            } else $login = false;
+        } else $login = false;
+
+        return $login;
+    }
+
+    function signUp($table, $fullname, $email, $username, $password)
+    {
+        $fullname = $this->prepareData($fullname);
+        $username = $this->prepareData($username);
+        $password = $this->prepareData($password);
+        $email = $this->prepareData($email);
+        $password = password_hash($password, PASSWORD_DEFAULT);
+        $this->sql =
+            "INSERT INTO " . $table . " (fullname, username, password, email) VALUES ('" . $fullname . "','" . $username . "','" . $password . "','" . $email . "')";
+        if (mysqli_query($this->connect, $this->sql)) {
+            return true;
+        } else return false;
+    }
+
+}
+
+?>
